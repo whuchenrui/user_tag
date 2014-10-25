6f4386674ed44ae0470d06510540bcd7ef8d86bb
ray <rayprogram@163.com>
2014-10-25 14:19:48 +0800

    修改bug,　完善tag统计

--- application/model/ModelUserTag.py
+++ application/model/ModelUserTag.py
@@ -2,6 +2,7 @@
 __author__ = 'ray'
 
 from MongoConn import MongoConn
+import chardet
 
 
 class ModelUserTag():
@@ -13,7 +14,7 @@ class ModelUserTag():
         self.col = self.db.cuser_tag
 
     def update(self, _idfa, _app, _record):
-        result = self.col.find({'_id': _idfa})
+        result = self.col.find_one({'_id': _idfa})
         if not result:
             # 插入一条新的记录, 记录排行前三的tag名称
             temp = {}
@@ -27,12 +28,11 @@ class ModelUserTag():
                 s = _record[tag][1]
                 f = _record[tag][2]
                 total = _record[tag][3]
-                #TODO: 获得最大的三个值有问题
                 if total > a:
-                    b = a
-                    bb = aa
                     c = b
                     cc = bb
+                    b = a
+                    bb = aa
                     a = total
                     aa = tag
                 elif total > b:
@@ -48,7 +48,6 @@ class ModelUserTag():
             temp['like'] = [aa, bb, cc]
             temp['tag'] = str_tag
             self.col.insert(temp)
-
         else:
             a, b, c = 0, 0, 0
             aa, bb, cc = '', '', ''
@@ -57,42 +56,42 @@ class ModelUserTag():
             str_tag = ''
             temp['app'] = _app
             temp['_id'] = _idfa
-            list_tag = result['tag'].strip('&&').split('&&')
+            mongo_tag = result['tag'].encode('utf-8')
+            list_tag = mongo_tag.strip('&&').split('&&')
             for i in range(0, len(list_tag)):
                 info = list_tag[i].split('==')
                 data_info  = [int(info[1]), int(info[2]), int(info[3]), int(info[4])]
                 dict_tag[info[0]] = data_info
+
             for tag in _record:
                 if tag in dict_tag:
-                    for i in range(0, 4):
-                        dict_tag[tag][0] += _record[tag][0]
-                        dict_tag[tag][1] += _record[tag][1]
-                        dict_tag[tag][2] += _record[tag][2]
-                        dict_tag[tag][3] += _record[tag][3]
-                        total = dict_tag[tag][3]
-                        if total > a:
-                            b = a
-                            bb = aa
-                            c = b
-                            cc = bb
-                            a = total
-                            aa = tag
-                        elif total > b:
-                            c = b
-                            cc = bb
-                            b = total
-                            bb = tag
-                        elif total > c:
-                            c = total
-                            cc = tag
+                    dict_tag[tag][0] += _record[tag][0]
+                    dict_tag[tag][1] += _record[tag][1]
+                    dict_tag[tag][2] += _record[tag][2]
+                    dict_tag[tag][3] += _record[tag][3]
+                    total = dict_tag[tag][3]
+                    if total > a:
+                        c = b
+                        cc = bb
+                        b = a
+                        bb = aa
+                        a = total
+                        aa = tag
+                    elif total > b:
+                        c = b
+                        cc = bb
+                        b = total
+                        bb = tag
+                    elif total > c:
+                        c = total
+                        cc = tag
                 else:
                     dict_tag[tag] = _record[tag]
+            temp['like'] = [aa, bb, cc]
             for tag in dict_tag:
                 v, s, f, total = dict_tag[tag][0], dict_tag[tag][1], dict_tag[tag][2], dict_tag[tag][3]
                 tag_info = str(v)+'=='+str(s)+'=='+str(f)+'=='+str(total)
-                str_tag += '&&' + tag + '==' +tag_info
-            temp['like'] = [aa, bb, cc]
+                str_tag += '&&' + tag + '==' + tag_info
             temp['tag'] = str_tag
-
             self.col.remove({'_id': _idfa})
             self.col.insert(temp)
