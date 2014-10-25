a5fc2e3b44731985dfab78de005893fe4d5bbc5f
whuchenrui <rayprogram@163.com>
2014-10-25 21:29:10 +0800

    增加check_is_counted函数, 判断是否已经计算过

--- application/model/ModelLog.py
+++ application/model/ModelLog.py
@@ -6,10 +6,31 @@ from MongoConn import MongoConn
 class ModelLog():
     db = None
     col = None
+    status = None
 
     def __init__(self, _log_time):
         self.db = MongoConn('log', _log_time)
         self.col = self.db.clog_info
+
+
+    def check_is_counted(self, _log_time):
+        self.status = self.db.cstatue
+        result = self.status.find_one({'type': 'tag'})
+        if result:
+            list_time = result['time']
+            if _log_time in list_time:
+                return True
+            else:
+                list_time.append(_log_time)
+                self.status.update({'type': 'tag'}, {'$set': {'time': list_time}})
+                return False
+        else:
+            temp = {}
+            temp['type'] = 'tag'
+            temp['time'] = [_log_time]
+            self.status.insert(temp)
+            return False
+
 
     def get_user_pics(self):
         dict_idfa = {}
