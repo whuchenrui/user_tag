86eaea00c47f9ed49dcc82830df7ec6e97765c6d
whuchenrui <rayprogram@163.com>
2014-10-25 21:29:44 +0800

    增加db_status数据表的连接

--- application/model/MongoConn.py
+++ application/model/MongoConn.py
@@ -8,10 +8,12 @@ class MongoConn():
     conn = None
     clog_info = None
     cuser_tag = None
+    cstatue = None
 
     def __init__(self, _type, _log_time=None):
         self.client = pymongo.Connection()
         self.conn = self.client.db_wallpaper
+        self.cstatue = self.conn.db_status
         if _type == 'log':
             col_name = _log_time + '_log'
             self.clog_info = self.conn[col_name]
