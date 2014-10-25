8a68b86bc7785fc8c87f00b4618373bc6a355f2f
ray <rayprogram@163.com>
2014-10-25 14:18:18 +0800

    去除验证数据的函数，移动到test目录下

--- application/core/RedisData.py
+++ application/core/RedisData.py
@@ -1,1 +1,27 @@
+# coding=utf-8
 __author__ = 'ray'
+
+import codecs
+from application.model.RedisConn import RedisConn
+from datetime import datetime
+import chardet
+
+def make_data():
+    fin = codecs.open('../../data/pictures.csv', 'r', encoding='UTF-8')
+    redis_conn = RedisConn()
+    while True:
+        line = fin.readline()
+        if not line:
+            break
+        list_line = line.split('\t')
+        pid = list_line[0]
+        list_tag = list_line[1].split(',')
+        for i in range(0, len(list_tag)):
+            redis_conn.cache.lpush(pid, list_tag[i])
+    fin.close()
+
+
+
+
+if __name__ == '__main__':
+    make_data()
